68ba022f70e93070927234552ca45a46bfd98167
Santiago Roa <santiagoroa98@hotmail.com>
2020-11-25 19:17:32 -0500

    Actualización 9

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const facil = [
     "685329174971485326234761859362574981549618732718293465823946517197852643456137298"
 ];
 const normal = [
-    "--9-------4----6-758-31----15--4-36-------4-8----9-------75----3-------1--2--3--",
+    "--9-------4----6-758-31----15--4-36-------4-8----9-------75----3-------1--2--3---",
     "619472583243985617587316924158247369926531478734698152891754236365829741472163895"
 ];
 const dificil = [
@@ -17,7 +17,7 @@ const limpio = [
 ];
 
 var cronometro = null;
-var inicio = false;
+var inicio;
 var hayDificultad = false;
 var mSegundos = 0;
 var errores = 0;
@@ -28,21 +28,21 @@ var minutos;
 var segundos;
 var texto_minutos;
 var texto_segundos;
+var datos = [];
 
 
-window.onload = function () {
-    //Funciones a cargar después de la carga de la página
+window.onload = function () { //Funciones a cargar después de la carga de la página
     generarTablero(limpio[0]);
     cargarCronometro();
     cargarBotones();
     cargarNumeros();
 }
 
-function cargarNumeros() {
+function cargarNumeros() { //Función que carga los números y asigna los eventos
     for (let i = 0; i < id("contenedor_num").children.length; i++) {
         id("contenedor_num").children[i].addEventListener("click", function () {
             // Seleccionar un número a la vez
-            if (!desSeleccion) {
+            if (!desSeleccion && comprobarInicio() == true) {
                 //Comprobar si número ya está seleccionado
                 if (this.classList.contains("seleccionado")) {
                     this.classList.remove("seleccionado");
@@ -55,12 +55,12 @@ function cargarNumeros() {
                     numSeleccionado = this;
                     actualizarMovimiento();
                 }
-            }
+            } else alert("Primero debe iniciar un nuevo juego!");
         });
     }
 }
 
-function cargarBotones() {
+function cargarBotones() { //Función que asigna eventos a los botones
     //Se añade eventListener a cada boton
     id("boton_iniciar").addEventListener("click", iniciarJuego);
     id("boton_comprobar").addEventListener("click", comprobar);
@@ -68,14 +68,14 @@ function cargarBotones() {
     id("boton_reiniciar").addEventListener("click", reiniciar);
 }
 
-function cargarCronometro() {
+function cargarCronometro() { //Función que carga variables de cronómetro
     texto_minutos = id("t_minutos");
     minutos = Number(texto_minutos.innerHTML);
     texto_segundos = id("t_segundos");
     segundos = Number(texto_segundos.innerHTML);
 }
 
-function iniciarJuego() {
+function iniciarJuego() { //Función que iniciar una nueva partida
     //Se comprueba si un juego está en curso
     if (!comprobarInicio()) {
         inicio = true;
@@ -94,28 +94,48 @@ function iniciarJuego() {
     } else alert("Seleccione la opción 'Reiniciar' para iniciar una nueva partida");
 }
 
-function comprobar() {
+function comprobar() { //Función que comprueba si el tablero actual es igual a la solución
+    validarSudoku();// Se recogen los números guardados en el sudoku
     //Se comprueba que exista una partida
     if (!comprobarInicio()) alert("Primero debe iniciar un nuevo juego!");
     else {
         //Se comprueba si la solución es válida
+        if (datos == facil[1] || datos == normal[2] || datos == dificil[1]) {
+            alert("Correcto, ha ganado!");
+            inicio = false;
+            detenerCronometro();
+        } else {
+            alert("Incorrecto, siga intentando");
+            //Se registra un error
+            if (id("dif_normal").checked || id("dif_dificil").checked) {
+                errores++;
+                actualizarErrores();
+            }
+        }
     }
 }
 
-function comprobarInicio() {
+function comprobarInicio() { //Función que comprueba si hay una partida en curso
     return inicio;
 }
 
-function reiniciar() {
+function actualizarErrores() {
+    cambiarTexto("errores", errores);
+}
+
+function reiniciar() { //Función que reestablece el juego
     inicio = false;
     hayDificultad = false;
+    errores = 0;
+    datos = [];
     limpiarTablero();
     resetearCronometro();
+    actualizarErrores();
     mostrarDificultad();
     generarTablero(limpio[0]);
 }
 
-function generarTablero(tablero) {
+function generarTablero(tablero) { //Función que crea el tablero
     //Se limpian los tableros
     limpiarTablero();
     //Crear tablero
@@ -131,7 +151,7 @@ function generarTablero(tablero) {
             //Añadir eventListener a la celda
             celda.addEventListener("click", function () {
                 //Si se puede seleccionar
-                if (!desSeleccion) {
+                if (!desSeleccion && comprobarInicio() == true) {
                     //Si la celda ya está seleccionada
                     if (celda.classList.contains("seleccionado")) {
                         //Remover seleccion
@@ -147,6 +167,8 @@ function generarTablero(tablero) {
                         celdaSeleccionada = celda;
                         actualizarMovimiento();
                     }
+                } else {
+                    alert("Primero debe iniciar un nuevo juego!");
                 }
             });
         }
@@ -167,40 +189,67 @@ function generarTablero(tablero) {
     }
 }
 
-function actualizarMovimiento() {
-    //Se comprueba si una celda y un número y una celda están seleccionados
+function actualizarMovimiento() { //Función que comprueba y posiciona los números en la celda
+    //Si una celda y número están seleciconados
     if (celdaSeleccionada && numSeleccionado) {
-        // Rellenar la celda con el número seleccionado
+        // Poner el número seleccionado en la celda
         celdaSeleccionada.textContent = numSeleccionado.textContent;
-        // Si el número seleccionado coincide con la solución
-        if (comprobarCorrecto(celdaSeleccionada)) {
+        //Si la dificultad fácil está seleccionada
+        if (id("dif_facil").checked) {
+            asistirMovimiento();
+        } else {
             //Deseleccionar la celda
             celdaSeleccionada.classList.remove("seleccionado");
             numSeleccionado.classList.remove("seleccionado");
-            //Limpiar las variables seleccionadas
+            //Limpair las variables seleccionadas
             numSeleccionado = null;
-            celdaSeleccionada = false;
-        } else {
-            //if()
-            // Si el número no coincide con la solución
-            desSeleccion = true;
-            // Estilizar celda
-            celdaSeleccionada.classList.add("incorrecto");
-            setTimeout(function () {
-                errores++;
-                //Restaurar el color de la celda
-                celdaSeleccionada.classList.remove("incorrecto");
-                celdaSeleccionada.classList.remove("seleccionado");
-                numSeleccionado.classList.remove("seleccionado");
-                //Limpiar la celda y limpiar las variables
-                celdaSeleccionada.textContent = "";
-                celdaSeleccionada = null;
-            }, 1000);
+            celdaSeleccionada = null;
         }
     }
 }
 
-function comprobarCorrecto(celda) {
+function asistirMovimiento() { //Función que indica al usuario si un movimiento es incorrecto
+    //Si el número corresponde a la solución
+    if (comprobarCeldaCorrecta(celdaSeleccionada)) {
+        //Deseleccionar la celda
+        celdaSeleccionada.classList.remove("seleccionado");
+        numSeleccionado.classList.remove("seleccionado");
+        //Limpair las variables seleccionadas
+        numSeleccionado = null;
+        celdaSeleccionada = null;
+    } else { //Si el número no corresponde a la solución
+        errores++;
+        desSeleccion = true;
+        //Estilizar celda
+        celdaSeleccionada.classList.add("incorrecto");
+        //Esperar 1 seg
+        setTimeout(function () {
+            //Reactivar selección
+            desSeleccion = false;
+            //Restaurar estilo
+            celdaSeleccionada.classList.remove("incorrecto");
+            celdaSeleccionada.classList.remove("seleccionado");
+            numSeleccionado.classList.remove("seleccionado");
+            //Limpiar texto
+            celdaSeleccionada.textContent = "";
+            celdaSeleccionada = null;
+            numSeleccionado = null;
+            //Registrar error
+            actualizarErrores();
+        }, 1000);
+    }
+}
+
+//Función que recoge los numeros del sudoku y los almacena en un arreglo
+function validarSudoku() {
+    let numCelda;
+    for (let i = 0; i < 81; i++) {
+        numCelda = id(i.toString()).textContent;
+        datos[i] = numCelda;
+    }
+}
+
+function comprobarCeldaCorrecta(celda) { //Función que comprueba si una celda corresponde a la solución
     //Ajustar solución basada en la dificultad
     let solucion;
     if (id("dif_facil").checked) solucion = facil[1];
@@ -211,7 +260,7 @@ function comprobarCorrecto(celda) {
     else return false;
 }
 
-function mostrarDificultad() {
+function mostrarDificultad() { //Función que muestra/oculta los niveles de dificultad
     let dFacil = id("boton_facil");
     let dNormal = id("boton_normal");
     let dDificil = id("boton_dificil");
@@ -221,7 +270,6 @@ function mostrarDificultad() {
             dNormal.style.visibility = 'hidden';
             dDificil.style.visibility = 'hidden';
         } else if (id("dif_normal").checked) {
-            console.log("entro");
             dFacil.style.visibility = 'hidden';
             dDificil.style.visibility = 'hidden';
         } else {
@@ -236,7 +284,7 @@ function mostrarDificultad() {
     }
 }
 
-function reestablecer() {
+function reestablecer() { //Función que limpia los números ingresados en el tablero
     if (inicio) {
         let tablero;
         if (id("dif_facil").checked) tablero = facil[0];
@@ -248,7 +296,7 @@ function reestablecer() {
     }
 }
 
-function limpiarTablero() {
+function limpiarTablero() { //Función que vacía el tablero por completo
     let celdas = qsa(".celda"); //Acceder a las celdas
     //Limpiar las celdas
     for (let i = 0; i < celdas.length; i++) {
@@ -263,6 +311,7 @@ function limpiarTablero() {
     numSeleccionado = null;
 }
 
+//Cronómetro
 function obtenerTiempo(iniciarTiempo) {
     let tiempoAcumulado = 0;
     if (iniciarTiempo > 0) {
@@ -305,12 +354,22 @@ function iniciarCronometro() {
             segundos++;
             iniciarCronometro();
         }
+        //Comprueba si han pasado 5 minutos en dificultad Difícil
+        if (id("dif_dificil").checked && minutos > 5) {
+            //Detiene el cronómetro y acaba el juego
+            minutos = 5;
+            alert("Se acabó el tiempo!");
+            detenerCronometro();
+            inicio = false;
+        }
     }, 10);
 }
+
 function detenerCronometro() {
     cronometro = pararContador(cronometro);
     return true;
 }
+
 function resetearCronometro() {
     cronometro = pararContador(cronometro);
     segundos = 0;
@@ -319,6 +378,20 @@ function resetearCronometro() {
     cambiarTexto("t_segundos", "00");
     return true;
 }
+
+function pararContador(cronometro) {
+    if (cronometro) {
+        clearInterval(cronometro);
+        return cronometro;
+    }
+    else return cronometro;
+}
+
+function iniciarContador() {
+    var tiempoActual = new Date();
+    return tiempoActual.getTime();
+}
+//Fin cronómetro
 
 //Funciones de miscelanea
 
@@ -337,17 +410,4 @@ function qs(selector) {
 
 function qsa(selector) {
     return document.querySelectorAll(selector);
-}
-
-function pararContador(cronometro) {
-    if (cronometro) {
-        clearInterval(cronometro);
-        return cronometro;
-    }
-    else return cronometro;
-}
-
-function iniciarContador() {
-    var tiempoActual = new Date();
-    return tiempoActual.getTime();
 }
